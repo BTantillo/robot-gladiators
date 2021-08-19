51cfee40101fbad83e147a115938dfdd79e1b8b7
Mountaineer Ben <bentantillo@gmail.com>
2021-08-18 18:47:44 -0700

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -37,7 +37,7 @@ var fight = function(enemyName) {
         if (confirmSkip) {
         window.alert(playerName + " has decided to skip this fight. Goodbye!");
         //subtract money from playerMoney for skipping
-        playerMoney = playerMoney -10;
+        Math.max(0, playerMoney -10) ;
         console.log("playerMoney" , playerMoney);
         break;
         }
@@ -46,7 +46,10 @@ var fight = function(enemyName) {
     //if player choses to fight, then fight
     if (promptFight === "fight" || promptFight === "FIGHT") {
     //Subtract the value of 'playerAttack from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable.
-    enemyHealth = enemyHealth - playerAttack;
+    //generate random damage value based on player's attack power
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+
+    enemyHealth = Math.max(0, enemyHealth - playerAttack);
     console.log(
     playerName + " attacked " + enemyName + ". " + enemyName + " now had " + enemyHealth + " health remaining." );
 
@@ -60,7 +63,10 @@ var fight = function(enemyName) {
     window.alert(enemyName + " still has " + enemyHealth + " health left.");
     }
     //Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth'variable.
-    playerHealth = playerHealth - enemyAttack;
+    
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+    playerHealth = Math.max(0, playerHealth - damage);
     //Log a resulting message to the console so we know that it worked.
     console.log(
         enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
@@ -91,7 +97,7 @@ var fight = function(enemyName) {
         window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ));
         
         var pickedEnemyName = enemyNames[i];
-            enemyHealth = 50;
+            enemyHealth = randomNumber(40, 60);
             fight(pickedEnemyName);
 
             //if player is still alvie and we're not at the last enemy in the array
@@ -187,6 +193,13 @@ var shop = function() {
     break;
     }
 };
+
+//function to generate a random numeric value 
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+    return value;
+
+}
 
 
 
